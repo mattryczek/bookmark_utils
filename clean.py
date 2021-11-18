3563f306a4247e494fd419e6e906e022632902cd
Mateusz Ryczek <mateusz@u.northwestern.edu>
2021-11-18 00:10:45 -0600

    Minor UI Tweaks

--- clean.py
+++ clean.py
@@ -2,13 +2,13 @@ import sys, json
 
 filename = str(sys.argv[1])
 
-print(f"\n[OK] Filename: {filename}")
+print(f"\n[OK] Using {filename}")
 
 try:
     f = open(filename, encoding='utf8')
     print("[OK] File read successful")
 except:
-    print("Could not open file")
+    print("[FATAL] Could not open file")
     exit()
 
 data = json.load(f)
@@ -17,20 +17,21 @@ folders = data['children']
 clean = set()
 
 print("[INFO] Found folders:\n")
+print(f"{filename}:")
 
 for folder in folders:
     try:
         count = len(folder['children'])
         for bookmark in folder['children']:
             clean.add(bookmark['uri'])
-            
+
         count = str(count) + f", {count - len(clean)} duplicates"
 
     except:
         # If a folder has no items there is no 'children' key
         count = 0
 
-    print(f"{folder['title']} [{count}]")
+    print(f"  📂 {folder['title']} [{count}]")
     clean = set()
 
 print()
